82803b74000f44f174887407fc3badfd03806cb4
Catherine <cat.taylor.taylot@btinternet.com>
2017-10-24 13:17:30 +0100

    Began grabcut function

---
+++ grabcut.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include<string>
+
+int main(int aargc, char** argv){
+
+    return 0;
+}
