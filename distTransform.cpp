ea62b4e20e4822a630d978a79207fb67fd598a59
Catherine <cat.taylor.taylot@btinternet.com>
2017-10-25 14:48:14 +0100

    Fixed indenting

--- distTransform.cpp
+++ distTransform.cpp
@@ -22,34 +22,25 @@ int main(){
     }
   
 
-    Mat img_grey, dist;
+    Mat img_grey, dist, canny_edges;
     cvtColor(image, img_grey, CV_BGR2GRAY);
     blur( img_grey, img_grey, Size(3,3) );
 
-        Mat canny_output;
-        std::vector<std::vector<Point> > contours;
-        std::vector<Vec4i> hierarchy;
+    std::vector<std::vector<Point> > contours;
+    std::vector<Vec4i> hierarchy;
+    Canny( img_grey, canny_edges, contourThreshold, contourThreshold*2, 3 );
+    findContours( canny_edges, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
       
-        /// Detect edges using canny
-        Canny( img_grey, canny_output, contourThreshold, contourThreshold*2, 3 );
-        /// Find contours
-        findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-      
-        /// Draw contours
-        Mat drawing = 255*Mat::ones( canny_output.size(), CV_8UC1 );
-        for( int i = 0; i< contours.size(); i++ )
-           {
-            // Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-             drawContours( drawing, contours, i, Scalar(0,0,0), 2, 8, hierarchy, 0, Point() );
-           }
-           
+    Mat drawing = 255*Mat::ones( canny_edges.size(), CV_8UC1 );
+    for( int i = 0; i< contours.size(); i++ ){
+        drawContours( drawing, contours, i, Scalar(0,0,0), 2, 8, hierarchy, 0, Point() );
+    }
+    distanceTransform(drawing, dist, CV_DIST_L2, 3); //apply distance transfrom
+    normalize(dist, dist, 0, 1., NORM_MINMAX);
 
-
-      distanceTransform(drawing, dist, CV_DIST_L2, 3); //apply distance transfrom
-       normalize(dist, dist, 0, 1., NORM_MINMAX);
     char exit_key_press = 0;
     do {
-      imshow("myWindow",dist);
+        imshow("myWindow",dist);
         exit_key_press = cvWaitKey(1);
     }while (exit_key_press != '\x1b');
 
