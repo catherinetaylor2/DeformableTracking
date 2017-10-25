9f61c63df9243267f090b190d676f790b85d4dfa
Catherine <cat.taylor.taylot@btinternet.com>
2017-10-25 15:46:46 +0100

    removed unneeded contour calculations

--- distTransform.cpp
+++ distTransform.cpp
@@ -6,8 +6,7 @@
 
 using namespace cv;
 
-int contourThreshold = 255;
-float thresh = 0.2f;
+float thresh = 0.1f;
 
 
 
@@ -22,33 +21,19 @@ int main(){
         std::cerr<<"Error: cannot open image \n";
     }
   
-    Mat img_grey, dist, canny_edges;
-    cvtColor(image, img_grey, CV_BGR2GRAY);
-    blur( img_grey, img_grey, Size(3,3) );
-
-    std::vector<std::vector<Point> > contours;
-    std::vector<Vec4i> hierarchy;
-    Canny( img_grey, canny_edges, contourThreshold, contourThreshold*2, 3 );
-    findContours( canny_edges, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-      
-    Mat drawing = 255*Mat::ones( canny_edges.size(), CV_8UC1 );
-    for( int i = 0; i< contours.size(); i++ ){
-        drawContours( drawing, contours, i, Scalar(0,0,0), 2, 8, hierarchy, 0, Point() );
-    }
-
-    Mat bw;
+    Mat dist, bw;
     cvtColor(image, bw, CV_BGR2GRAY);
     threshold(bw, bw, 10, 100, CV_THRESH_BINARY | CV_THRESH_OTSU);
 
-   distanceTransform(bw, dist, DIST_L2, 3); //apply distance transfrom
+  distanceTransform(bw, dist, DIST_L2, 3); //apply distance transfrom
   normalize(dist, dist, 0, 1., NORM_MINMAX);
 
     for(int x = 0; x<dist.rows; x++){
         for (int y =0; y<dist.cols; y++){
-           if(dist.at<float>(x, y)>(0.2)){
+           if(dist.at<float>(x, y)>(0.05 + thresh)){
                dist.at<float>(x, y)= 0;
             }
-            else if(dist.at<float>(x, y)<(0.1)){
+            else if(dist.at<float>(x, y)<(thresh - 0.05)){
                 dist.at<float>(x, y)= 1;
             }
             else{
